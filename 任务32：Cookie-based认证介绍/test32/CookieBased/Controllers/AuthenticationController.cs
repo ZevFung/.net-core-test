b0890d89b3d30e78271f200eb507046ff0b29b1c
zevfung <1029541159@qq.com>
2018-12-07 20:44:41 +0800

    32

---
+++ 任务32：Cookie-based认证介绍/test32/CookieBased/Controllers/AuthenticationController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace ChatRoom.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name,"fzw"),
+                new Claim(ClaimTypes.Role,"admin")
+            };
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+            return Ok();
+        }
+
+
+        public IActionResult Logout()
+        {
+            HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return Ok();
+        }
+    }
+}
